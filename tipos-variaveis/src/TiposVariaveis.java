8178fa8e6db899ca5ee700ffc2d0b8722397e802
layzasilva <kermilynlay@gmail.com>
2024-07-02 08:52:09 -0300

    2º commit - aprendendo sobre variaveis e 'cast'

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,13 @@
 public class TiposVariaveis {
     
+    public static void main (String[]args) throws Exception{
+        double salarioMinimo = 3600;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        
+
+    }
 }
