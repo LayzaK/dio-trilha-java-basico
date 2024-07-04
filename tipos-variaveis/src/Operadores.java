ea8260679e473a08383790bc6558bd4b4a1bc494
layzasilva <kermilynlay@gmail.com>
2024-07-04 09:01:04 -0300

    7º commit - incrementação e estrutura if/else

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,8 +2,39 @@ public class Operadores {
 
     public static void main (String[] args) {
         
+        //Operadores, tipos e como usa-los
+
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
+
+        //x repetição
+        int numero = 5;
+
+        //numero = numero +1
+
+        System.out.println(++ numero);
+
+        System.out.println(numero);
+
+        //condicional if/else
+        int a,b;
+
+        a = 5;
+        b = 10;
+
+        String resultado = "";
+        if (a==b)
+            resultado = "verdadeiro";
+        else 
+            resultado = "falso";
+
+        System.out.println(resultado);
+
+
+
+
+
+
 
     }
     
