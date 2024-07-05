5456bb75e2b83471c3c8ce34eebf4ac5b8b4f6ef
layzasilva <kermilynlay@gmail.com>
2024-07-05 08:55:35 -0300

    9º commit - adicionando instrução e criando uma

---
+++ tipos-variaveis/src/Instruções.java
@@ -0,0 +1,19 @@
+public class Instruções {
+
+    public static void main (String[] args){
+
+        chuu(20, 30);
+
+       
+
+        }
+
+        static void chuu(int x, int y){
+
+            System.out.println(x + y);
+            System.out.println(x * y);
+
+
+    }
+    
+}
