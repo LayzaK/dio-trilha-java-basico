8dff7a2534243a69d2f78cf840af6bdf61c1a0f7
layzasilva <kermilynlay@gmail.com>
2024-07-05 08:59:59 -0300

    10º commit - adicionando comentários sobre instruções

--- tipos-variaveis/src/Instruções.java
+++ tipos-variaveis/src/Instruções.java
@@ -2,13 +2,17 @@ public class Instruções {
 
     public static void main (String[] args){
 
-        chuu(20, 30);
+        chuu(20, 30); //argumentos
 
        
 
         }
 
         static void chuu(int x, int y){
+
+            //static void -> declaração
+            //() -> utilizamos para definir o que chegará naquela função(paramentros e logo após argumentos)
+            //{} -> o código fica dentro dessas chaves  
 
             System.out.println(x + y);
             System.out.println(x * y);
